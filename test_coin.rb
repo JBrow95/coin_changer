8d406aa090a2fdcbc3358facc0f0246d1be8d7c8
JBrow95 <browningjustin95@gmail.com>
2018-07-11 10:43:05 -0400

    test failed, penny has no value

---
+++ test_coin.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require_relative "coin.rb"
+
+class TestCoin < Minitest::Test
+
+    def test_if_penny_equals_1
+        assert_equal({:penny => 1}, changer(1))
+    end
+
+end
