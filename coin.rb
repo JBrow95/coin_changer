62523de19ce5dc82c1729d0d29a5bb035161465f
JBrow95 <browningjustin95@gmail.com>
2018-07-11 11:50:04 -0400

    test passed

--- coin.rb
+++ coin.rb
@@ -1,3 +1,13 @@
 def changer(amount)
-	{:penny => 1}
+	change = {}
+	while amount > 0 do
+		if amount >= 5
+			change[:nickel] = 1
+		   amount -= 5		
+		elsif amount >= 1
+			change[:penny] = 1
+			amount -= 1
+		end
+	end
+	change
 end
--- test_coin.rb
+++ test_coin.rb
@@ -3,12 +3,19 @@ require_relative "coin.rb"
 
 class TestCoin < Minitest::Test
 
+	def test_if_hash_is_class
+		amount = 1
+		assert_equal(Hash, changer(amount).class)
+	end
+
     def test_if_penny_equals_1
-        assert_equal({:penny => 1}, changer(1))
+    	amount = 1
+        assert_equal({:penny => 1}, changer(amount))
     end
 
     def test_if_nickel_equals_5
-    	assert_equal({:nickel => 5}, changer(5))
+    	amount = 5
+    	assert_equal({:nickel => 1}, changer(amount))
     end
 
 end
